f6d7c9b55e5aa504119f137302a0d42dd6cf0dd4
Blaise Akinmolayan <patrykblaise@yahoo.com>
2025-07-22 23:43:00 -0400

    update backend.tf

--- prod/backend.tf
+++ prod/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "LockID"
-    bucket         = "blaise-09"
+    key            = "prod/terraform.state"
+    bucket         = "blaise09"
     region         = "us-east-2"
     dynamodb_table = "tf-state-locking01"
   }
