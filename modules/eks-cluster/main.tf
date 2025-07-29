b18c419e363a9d4902c14b81de9a64e871abddec
Blaise Akinmolayan <patrykblaise@yahoo.com>
2025-07-28 23:52:31 -0400

    update to module main.tf

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -107,7 +107,7 @@ module "eks" {
   access_entries = {
     fusi = {
       kubernetes_groups = ["eks-admins"]
-      principal_arn     = "arn:aws:iam::999568710647:user/nfusi"
+      principal_arn     = "arn:aws:iam::888577033820:user/Blaise"
       policy_associations = [
         {
           policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
@@ -118,7 +118,7 @@ module "eks" {
 
     github_runner = {
       kubernetes_groups = ["eks-admins"]
-      principal_arn     = "arn:aws:iam::999568710647:role/github-runner-ssm-role"
+      principal_arn     = "arn:aws:iam::888577033820:role/github-runner-ssm-role"
       policy_associations = [
         {
           policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
